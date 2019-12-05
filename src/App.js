9c13ddddc3db399a20a8b15c0c636e62665d2d5a
Simon Chow <Melanmare@users.noreply.github.com>
2019-12-05 20:31:22 +1100

    have a try of amplify

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         just want to figure out what will happen if i push sth to master branch
         </p>
         <a
           className="App-link"
