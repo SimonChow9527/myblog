61a4757d3e685284e94d3445c3e9ae6333156096
Simon Chow <Melanmare@users.noreply.github.com>
2020-01-08 13:09:49 +1100

    update text

--- src/components/homePagePieces/Projects.js
+++ src/components/homePagePieces/Projects.js
@@ -20,7 +20,7 @@ const Projects = () => {
             title="Personal Blog "
             role="Full-Stack developer | Dec 2019-Present"
             keyword="Keyword: React, React-Redux, .NET core, AWS"
-            desc="I've been working on this project since last December. It is a place not only to introduce myself but also to sharpen what I've learned: from design to implementation, from front-end to back-end, cloud, etc. And I also keep doing code refactoring and adding new features to make it looks even better."
+            desc="I've been working on this project since last December. It is a place not only to introduce myself but also to sharpen what I've learned: from design to implementation, from front-end to back-end, cloud deployment, etc. And I also keep doing code refactoring and adding new features to make it looks even better."
           />
         </li>
         <li className="homepage-project-content-li">
@@ -29,7 +29,7 @@ const Projects = () => {
             title="Anywhere search"
             role="Front-end developer | Jul 2019-Oct 2019 "
             keyword="Keyword: React, TypeScript, HTML5, SCSS, Redux-saga, Enzyme"
-            desc="Incididunt aute anim culpa consequat nostrud proident nostrud in ullamco laborum. Enim incididunt in cillum qui ad. Commodo id amet consequat nulla mollit veniam culpa enim officia proident labore nostrud ea. Nisi in irure elit velit deserunt."
+            desc="The goal for this Webjet sponsored internship project was to facilitate the user in choosing their next travel destination. And I worked as the front-end developer in a team of 6 people. My responsibilities were: Develop React components in Typescript, Utilize and refactor code provided, collaborate with UX designer and back-end developer, and report progress to the team. Through hard work and collaboration, the project turned out to be a huge success. "
           />
         </li>
       </ol>
